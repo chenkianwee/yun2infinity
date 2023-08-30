9958e98eadaaa2ebce1d6d8534dfbac64501d837
chenkianwee <chenkianwee@gmail.com>
2023-08-30 17:39:00 -0400

    viz 3dtiles 1.1

--- django/yun2inf_project/csviewer/static/cesium_script/csviz.js
+++ django/yun2inf_project/csviewer/static/cesium_script/csviz.js
@@ -48,7 +48,88 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
 viewer.baseLayerPicker.viewModel.terrainProviderViewModels.removeAll();
 const scene = viewer.scene;
 
-const tileset = await Cesium.Cesium3DTileset.fromUrl("http://localhost/static/3dtiles/TilesetWithRequestVolume/tileset.json");  
+const tileset = await Cesium.Cesium3DTileset.fromUrl("http://localhost/static/3dtiles/example/tileset.json");
 
 viewer.scene.primitives.add(tileset)
-viewer.flyTo(tileset);
+tileset.modelMatrix = Cesium.Transforms.eastNorthUpToFixedFrame(
+  Cesium.Cartesian3.fromDegrees(103.858459, 1.3452442559817817, 3.0)
+);
+
+viewer.zoomTo(tileset);
+
+tileset.featureIdLabel = 0;
+
+// Create an HTML element that will serve as the
+// tooltip that displays the feature information
+function createTooltip() {
+  const tooltip = document.createElement("div");
+  viewer.container.appendChild(tooltip);
+  tooltip.style.backgroundColor = "white";
+  tooltip.style.position = "absolute";
+  tooltip.style.left = "0";
+  tooltip.style.top = "0";
+  tooltip.style.padding = "14px";
+  tooltip.style["pointer-events"] = "none";
+  tooltip.style["block-size"] = "fit-content";
+  return tooltip;
+}
+const tooltip = createTooltip();
+
+// Show the given HTML content in the tooltip
+// at the given screen position
+function showTooltip(screenX, screenY, htmlContent) {
+  tooltip.style.display = "block";
+  tooltip.style.left = `${screenX}px`;
+  tooltip.style.top = `${screenY}px`;
+  tooltip.innerHTML = htmlContent;
+}
+
+// Create an HTML string that contains information
+// about the given feature, under the given title
+function createFeatureHtml(title, feature) {
+  if (!Cesium.defined(feature)) {
+    return `(No ${title})<br>`;
+  }
+  const propertyKeys = feature.getPropertyIds();
+  if (!Cesium.defined(propertyKeys)) {
+    return `(No properties for ${title})<br>`;
+  }
+  let html = `<b>${title}:</b><br>`;
+  for (let i = 0; i < propertyKeys.length; i++) {
+    const propertyKey = propertyKeys[i];
+    const propertyValue = feature.getProperty(propertyKey);
+    html += `&nbsp;&nbsp;${propertyKey} : ${propertyValue}<br>`;
+  }
+  return html;
+}
+
+// Given an object that was obtained via Scene#pick: If it is
+// a Cesium3DTileFeature, then it is returned.
+// Otherwise, 'undefined' is returned.
+function obtainFeature(picked) {
+  if (!Cesium.defined(picked)) {
+    return undefined;
+  }
+  const isFeature = picked instanceof Cesium.Cesium3DTileFeature;
+  if (!isFeature) {
+    return undefined;
+  }
+  return picked;
+}
+
+// Install the handler that will perform picking when the
+// mouse is moved, and update the label entity when the
+// mouse is over a Cesium3DTileFeature
+const handler = new Cesium.ScreenSpaceEventHandler(viewer.scene.canvas);
+handler.setInputAction(function (movement) {
+  let tooltipText = "";
+  const picked = viewer.scene.pick(movement.endPosition);
+
+  const feature = obtainFeature(picked);
+  tooltipText += createFeatureHtml("Feature", feature);
+
+  const screenX = movement.endPosition.x;
+  const screenY = movement.endPosition.y;
+  showTooltip(screenX, screenY, tooltipText);
+}, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
